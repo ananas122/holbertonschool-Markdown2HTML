143d715d0a4d50cb2cd1e211ca1d46fbed4fb68d
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-12-08 16:02:26 +0100

    To add comments

--- markdown2html.py
+++ markdown2html.py
@@ -9,7 +9,7 @@ import os
 
 
 def markdowntohtml():
-    """convert file md to html"""
+    """convert file md to html task 0"""
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
@@ -21,13 +21,22 @@ def markdowntohtml():
         print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
 
+    # task 1
     with open(markdown_file, 'r') as md, open(output_file, 'w') as html:
         for line in md:
             if line.startswith("#"):
+                # Calcule le niveau de l'en-tête en comptant le nombre de '#'
+                # par ex, '##' donne un lrvel de 2
                 level = len(line.split()[0])
+
+                # Enlève les caractères '#' et les espaces superflus de la ligne
+                # pour obtenir uniquement le texte de l'en-tête
                 content = line.lstrip("#").strip()
+
+                # crée une balise HTML d'en-tête correspondante
                 html.write(f"<h{level}>{content}</h{level}>\n")
             else:
+           
                 html.write(line)
 
 
