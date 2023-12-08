2630b5d457ecbd1a074e5363bcbab49ebadafd0c
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-12-08 10:26:14 +0100

    Task O

---
+++ markdown2html.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+import os
+
+
+def main():
+    # Vérifier si le nombre d'arguments est inférieur à 2
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    # Nom du fichier Markdown (premier argument)
+    markdown_file = sys.argv[1]
+    # Nom du fichier de sortie (deuxième argument)
+    output_file = sys.argv[2]
+
+    # Vérifier si le fichier Markdown existe
+    if not os.path.exists(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    # Si ok, terminer  sortie 0
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
