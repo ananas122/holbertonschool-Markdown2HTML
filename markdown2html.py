48c5709746971ab06f114254bffe254a377f2425
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-12-08 16:35:08 +0100

    task 2

--- markdown2html.py
+++ markdown2html.py
@@ -26,7 +26,7 @@ def markdowntohtml():
         for line in md:
             if line.startswith("#"):
                 # Calcule le niveau de l'en-tête en comptant le nombre de '#'
-                # par ex, '##' donne un lrvel de 2
+                # par ex, '##' donne un level de 2
                 level = len(line.split()[0])
 
                 # Enlève les caractères '#' et les espaces superflus de la ligne
@@ -36,9 +36,42 @@ def markdowntohtml():
                 # crée une balise HTML d'en-tête correspondante
                 html.write(f"<h{level}>{content}</h{level}>\n")
             else:
-           
+
                 html.write(line)
 
+        # Indicateur pour suivre si nous sommes dans une liste non ordonnée
+        in_unordered_list = False
 
+        for line in md:
+            if line.startswith("- "):
+                if not in_unordered_list:
+                    # Commence une nvl liste non ordonnée
+                    html.write("<ul>\n")
+                    in_unordered_list = True
+                # Ajoute l'élément de liste
+                content = line.lstrip("- ").strip()
+                html.write(f"    <li>{content}</li>\n")
+            else:
+                if in_unordered_list:
+                    # Ferme la liste non ordonnée si la ligne actuelle n'est plus un élément de liste
+                    html.write("</ul>\n")
+                    in_unordered_list = False
+                # Pour les en-têtes ou les autres lignes
+                if line.startswith("#"):
+                    level = len(line.split()[0])
+                    content = line.lstrip("#").strip()
+                    html.write(f"<h{level}>{content}</h{level}>\n")
+                else:
+                    html.write(line)
+
+        # Vérifie si nous devons fermer la liste à la fin du fichier
+        if in_unordered_list:
+            html.write("</ul>\n")
+
+
+               
+                
+        
+        
 if __name__ == "__main__":
     markdowntohtml()
