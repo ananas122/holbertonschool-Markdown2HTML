003f9492de0bdd73a0ceebc811b7409376d1fd5f
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-12-08 11:06:07 +0100

    Task 1

--- markdown2html.py
+++ markdown2html.py
@@ -6,7 +6,7 @@ Task 1: Ce script est destiné à convertir un fichier Markdown en fichier HTML.
 
 import sys
 import os
-import markdown
+
 
 def markdowntohtml():
     """convert file md to html"""
@@ -15,24 +15,21 @@ def markdowntohtml():
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
-    # Nom du fichier Markdown (premier argument)
+    # Nom du fichier Markdown (1er argument)
     markdown_file = sys.argv[1]
-    # Nom du fichier de sortie (deuxième argument)
+    # Nom du fichier de sortie (2eme argument)
     output_file = sys.argv[2]
 
-    # Vérifier si le fichier Markdown existe
     if not os.path.exists(markdown_file):
         print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
 
-    # Ajouter ici le code pour lire le fichier Markdown et le convertir en HTML
 
     # Si ok, terminer avec sortie 0
     sys.exit(0)
 
 
-def convert_heading(line):
-    """Convert heading line """
+
     if line.startswith("#"):
         level = len(line.split()[0])
         content = line.strip("#").strip()
