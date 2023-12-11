0f3310d762697d883c7f0202bce8a916d195d38e
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-12-11 20:53:46 +0100

    Task ul and ol

--- markdown2html.py
+++ markdown2html.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
 Markdown to HTML Converter
-Task 1: convert heading
+task
 """
 
 import sys
 import os
 
 
-def markdowntohtml():
-    """convert file md to html task 0"""
+def convertmdtohtml():
+    """ Main function that converts a Markdown file to an HTML file """
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
@@ -21,57 +21,45 @@ def markdowntohtml():
         print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
 
-    # task 1
-    with open(markdown_file, 'r') as md, open(output_file, 'w') as html:
-        for line in md:
-            if line.startswith("#"):
-                # Calcule le niveau de l'en-tête en comptant le nombre de '#'
-                # par ex, '##' donne un level de 2
-                level = len(line.split()[0])
+    try:
+        with open(markdown_file, 'r') as md, open(output_file, 'w') as html:
+            in_unordered_list = False
+            in_ordered_list = False
 
-                # Enlève les caractères '#' et les espaces superflus de la ligne
-                # pour obtenir uniquement le texte de l'en-tête
-                content = line.lstrip("#").strip()
-
-                # crée une balise HTML d'en-tête correspondante
-                html.write(f"<h{level}>{content}</h{level}>\n")
-            else:
-
-                html.write(line)
-
-        # Indicateur pour suivre si nous sommes dans une liste non ordonnée
-        in_unordered_list = False
-
-        for line in md:
-            if line.startswith("- "):
-                if not in_unordered_list:
-                    # Commence une nvl liste non ordonnée
-                    html.write("<ul>\n")
-                    in_unordered_list = True
-                # Ajoute l'élément de liste
-                content = line.lstrip("- ").strip()
-                html.write(f"    <li>{content}</li>\n")
-            else:
-                if in_unordered_list:
-                    # Ferme la liste non ordonnée si la ligne actuelle n'est plus un élément de liste
-                    html.write("</ul>\n")
-                    in_unordered_list = False
-                # Pour les en-têtes ou les autres lignes
+            for line in md:
                 if line.startswith("#"):
                     level = len(line.split()[0])
                     content = line.lstrip("#").strip()
                     html.write(f"<h{level}>{content}</h{level}>\n")
+                elif line.startswith("- "):
+                    if not in_unordered_list:
+                        html.write("<ul>\n")
+                        in_unordered_list = True
+                    content = line.lstrip("- ").strip()
+                    html.write(f"    <li>{content}</li>\n")
+                elif line.startswith("* "):
+                    if not in_ordered_list:
+                        html.write("<ol>\n")
+                        in_ordered_list = True
+                    content = line.lstrip("* ").strip()
+                    html.write(f"    <li>{content}</li>\n")
                 else:
+                    if in_unordered_list:
+                        html.write("</ul>\n")
+                        in_unordered_list = False
+                    if in_ordered_list:
+                        html.write("</ol>\n")
+                        in_ordered_list = False
                     html.write(line)
 
-        # Vérifie si nous devons fermer la liste à la fin du fichier
-        if in_unordered_list:
-            html.write("</ul>\n")
+            if in_unordered_list:
+                html.write("</ul>\n")
+            if in_ordered_list:
+                html.write("</ol>\n")
+    except IOError as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
 
 
-               
-                
-        
-        
 if __name__ == "__main__":
-    markdowntohtml()
+    convertmdtohtml()
